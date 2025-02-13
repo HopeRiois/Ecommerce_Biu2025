7b716665437540a0e430824a7b4b1f54743dedc2
Alvaro Rios <alvaroluisriosgarzon@gmail.com>
2025-02-12 23:13:13 -0500

    home top page

--- ecommerce/README.md
+++ ecommerce/README.md
@@ -6,6 +6,9 @@ Esta es la plataforma de eCommerce de la empresa soluciones AF.
 #Commit setUp
 Se crea los archivos del proyecto con Vuetify, se crean componentes para el proyecto y se configura la barra de navegación.
 
+#Commit home top page
+Se agregan imagenes de ejemplo para la maquetación del proyecto, se agrega un carrusel y un listado de articulos de referencia.
+
 -----
 
 Copyright (c) 2016-present Vuetify, LLC
--- ecommerce/src/pages/index.vue
+++ ecommerce/src/pages/index.vue
@@ -3,6 +3,45 @@
     <NavBar />
     <v-main>
       <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="12"
+          >
+            <v-carousel>
+              <v-carousel-item
+                v-for="(item, i) in items"
+                :key="i"
+                :src="item.src"
+                cover
+              >
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                  >
+                    <div class="d-flex fill-height justify-center align-center">
+                      <h2 class="text-white top">
+                        {{ item.title }}
+                      </h2>
+                      <br>
+                    </div>
+                    <div class="d-flex fill-height justify-center align-center">
+                      <p class="text text-wite">
+                        {{ item.text }}
+                      </p>
+                    </div>
+                    <div class="d-flex fill-height justify-center align-center">
+                      <v-btn style="margin-top: -120px;">
+                        Comprar
+                      </v-btn>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-carousel-item>
+            </v-carousel>
+          </v-col>
+        </v-row>
         test
       </v-container>
     </v-main>
@@ -12,3 +51,55 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue'
 </script>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: "7.jpg",
+          title: "Articulo 1",
+          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+        },
+        {
+          src: "1.jpg",
+          title: "Articulo 2",
+          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+        },
+        {
+          src: "2.jpg",
+          title: "Articulo 3",
+          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+        },
+        {
+          src: "3.jpg",
+          title: "Articulo 4",
+          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+        },
+        {
+          src: "4.jpg",
+          title: "Articulo 5",
+          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+        },
+        {
+          src: "5.jpg",
+          title: "Articulo 6",
+          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.top {
+  margin-top: 100px;
+}
+.text {
+  margin-top: -40px;
+  margin-left: 80px;
+}
+.top-day {
+  margin-top: 120px;
+}
+</style>
